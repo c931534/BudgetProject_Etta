dcd2cc12d23a29fef2c0d91f1b52a4fba227c89a
etta.huang <etta.huang@ailabpte.co>
2024-12-26 00:18:45 +0800

    invaild date range test green light

--- BudgetProject_Etta/Service/BudgetService.cs
+++ BudgetProject_Etta/Service/BudgetService.cs
@@ -7,6 +7,11 @@ public class BudgetService
    
     public decimal Query(DateTime startDate, DateTime endDate)
     {
+        // 判斷結束日期是否早於開始日期
+        if (endDate < startDate)
+        {
+            return 0;
+        }
         return -1;
     }
 }
